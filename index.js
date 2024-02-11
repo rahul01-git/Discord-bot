1feb32bedbd6626a41a5577eb43054b27754aba9
Rahul <rahulneupane0090@gmail.com>
2024-02-11 21:49:01 +0545

    adding cooldown

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+client.cooldowns = new Collection();
+
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
